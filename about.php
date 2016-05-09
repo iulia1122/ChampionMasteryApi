7631aca790fdf9e2863cfcf8be13160c3cae1f26
iulia1122 <nedelcuniulia@gmail.com>
2016-05-10 00:37:46 +0300

    Update about.php

--- about.php
+++ about.php
@@ -15,13 +15,14 @@
 <div class="header">
     <div class="container">
         <div class="nav">
-			<ul>
-				<li><a href="./">Home</a></li>
-				<li><a href="champions.php">Champion Information</a></li>
-				<li><a href="#" style="width:275px;  height:44px;"></a></li>
-				<li><a href="about.php">About</a></li>
-				<li><a href="https://github.com/iulia1122/ChampionMasteryApi/" target="_blank" style="border:none;">Github</a></li>
-			</ul>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Champion Information</a></li>
+                <li><a href="#" style="width:275px;  height:44px;"></a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Faq</a></li>
+                <li><a href="#" style="border:none;">Github</a></li>
+            </ul>
             <div class="logo">
 
             </div>
@@ -36,10 +37,13 @@
 		
 		<div class="cont_box">
 			<h2>About this Website</h2>
-			<p>	This website was created for the Riot Api Challenge, April 2016 on Champion Mastery data.  
-				Gaming is all about fun, luck and having a good strategy, so we decided to combine them into one website. We hope it will be  a fun experience for everyone that wishes to learn more about League of Legends, themselves and their friends.
-				
-		
+			<p>	This website was created for the Riot Api Challenge, April 2016 on Champion Mastery data. Gaming is all about fun, luck and 
+			having a good strategy, so we decided to combine them into one website. 
+			</br>We hope it will be a fun experience for everyone that wishes 
+			to learn more about League of Legends, themselves and their friends. As far as luck goes, we went on and got council from Lady Luck 
+			herself upon choosing the category, and as summoners (imnubcake and psycho), we were happy that faith has given us the opportunity to
+			work on our creativity/originality skills, and in our arsenal of spells and code creating magic, we used PHP/HTML/JQuery to bring chaos,
+			light and awe to the eyes and minds of any who dare enter our website, engulfing them better in the world of League of Legends.
 			</p>
 		</div>
 		<div class="heading">
@@ -47,7 +51,7 @@
 		</div>
 		<div class="clear"></div>
 		<div class="cont_box">
-		<p>The first step is selecting your server and typing in your summoner name. You can view your total champion mastery score in the panel below and even add up to four more of your friends.  </p>
+		<p style="width:94%;">The first step is selecting the realm(server) you are part of, and inscribing(typing) in your summoner name. You can view your total champion mastery score in the panel below and being a powerful summoner, you can summon 4 other friends to view their scores as well. </p>
 		<img class="search_img" src="image/search.png"/>
 		</div>	
 		<div class="cont_box">
@@ -70,7 +74,7 @@
 			
 			<div class="box_left2">
 			<h3> 2. Role </h3>
-			<p>This tab contains the most popular roles you and your friends like to play. Is your friend insisting you play as support? No problem, just show him your were made to play as an Assassin! </p></div>
+			<p>This tab reveals the most popular roles you and your companions like to partake. One of companions tries to persuade you into being a support? Destroy his dreams by showing him that you were an assassin in disguise all along. </p></div>
 			
 		</div>
 		<div class="cont_box1">
@@ -85,7 +89,7 @@
 			<div class="box_left2">
 			
 			<h3> 4. Ranked </h3>
-			<p> The title says it all. This is where you find information about Ranked Games. :) </p></div>
+			<p> Tired of the same all normal battles where you are not rated for your skills on the battlefield? Try the ranked matches, and you can find everything you need about them in the ranked tab. </p></div>
 			
 		</div>
 		
@@ -94,15 +98,16 @@
 		Champion Information
 		</div>
 		<div class="cont_box2">
-		<div class="subtitle">
+		
 
 			<p>This is where the magic begins. We added class filters to the champion list, just click one of the seven buttons( All, Support, Assassin. Tank, Fighter, Mage, Marksman) and enjoy the show.
 			Each champion panel contains basic information aswell as skills, skill description and skins. 
 			
-			Hint: You can click on skill icons and skins, try it and you might have a pleasant surprise. :D</p>
-			</div>
+			</br></br><span>Hint 1:</span> You can click on skill icons and skins, try it and you might have a pleasant surprise. :D</p>
+			<span>Hint 2:</span> The champion information panel is also accessible directly from the the champions tab.</p>	
+			<div class="image_center"><img src="image/champio.png" style="margin-top:30px;"/></div>
 			
-			<div class="image_center"><img src="image/champio.png"/></div>
+			<p class="red"> Made with &nbsp;<i class="fa fa-heart" aria-hidden="true"></i>&nbsp; for Riot Api Challenge </p>
 		</div>
 		
 		
